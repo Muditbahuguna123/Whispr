fee6dee75d00f8d7c4c813f3718a6caf07003254
Mudit Bahuguna <bahugunamk123@gmail.com>
2025-12-22 19:46:47 +0530

    created express server and backend

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require("http");
+const {server} = require("socket.io") //“From the socket.io package, take only the thing called Server.”
+
+const app = express();
+const server = http.createServer(app);
+/*Why is this needed?
+Express alone cannot handle WebSockets
+Socket.io needs an actual HTTP server*/
+
+//creating socket.io server ↓ . Attach Socket.io to the HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: "*"              //“Allow frontend to connect from ANY website.”
+  }
+});
+
+// these functions are called callback functions
+//A callback function is a function that you give to another function, so it can be called later.
+io.on("connection", (socket) => {
+    console.log("User connected: ", socket.id);
+
+    socket.on("send-message", (encryptedMessage)=>{
+        socket.broadcast.emit("receive-message", encryptedMessage);
+    })
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected: ", socket.id);
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log("Server running on http://localhost:3000");
+})
